1ac0fbde07e839f413f317e3cb581e431d9fa6c0
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-04-12 22:40:24 +0200

    delete babelrc file as its already in webpack module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 // import './css/normalize.css';
 // import './css/style.css';
-const person = {
-  name: 'John',
-  getName() {
-    return this.name;
-  },
-};
-
-const name = person?.getName?.();
